1edd9ec3044c64d95184d65598beff171445213b
witt <fgwang.660@gmail.com>
2026-04-21 16:25:35 +0800

    Update Cask/tiny-rdm.rb 1.2.6

--- Casks/tiny-rdm.rb
+++ Casks/tiny-rdm.rb
@@ -1,7 +1,7 @@
 cask "tiny-rdm" do
-  version "1.2.5"
-  sha256 arm:   "0918611c8a96dbbda5a8e59c4c810659bca5033d82f780df865d4d1dfed25b8a",
-         intel: "4415ff1bb28603c2bd18b57700ef6f515098bfd0d6b9b61f5aaccadc378a3c33"
+  version "1.2.6"
+  sha256 arm:   "6a9de72ebb6be5b4fce1ebec3186edf8bd1ed5e3221bede082479c6ffe03a61b",
+         intel: "ac69281ec7d943d9e315a87c152a02ccca3da4cbf030c55db53f2504aa3f7898"
 
   on_intel do
     url "https://github.com/tiny-craft/tiny-rdm/releases/download/v#{version}/TinyRDM_#{version}_mac_intel.dmg"
