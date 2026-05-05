f38f169509cb58f070e5b806edaa106b429e5403
witt <fgwang.660@gmail.com>
2026-05-05 19:28:23 +0800

    Update Cask/todesk.rb 4.8.8.9

--- Casks/todesk.rb
+++ Casks/todesk.rb
@@ -1,19 +1,30 @@
 cask "todesk" do
-  version "4.7.8.3"
-  sha256 "f1f5a3cb75390418e8e98064022a331aae2810fdff56891093a7aa5766c8ba36"
+  version "4.8.8.9"
+  sha256 :no_check
 
-  url "https://dl.todesk.com/macos/ToDesk_#{version}.pkg",
-      user_agent: :fake
+  url "https://www.todesk.com/download.html" # 占位用
+
   name "ToDesk"
   desc "个人版远程控制软件"
   homepage "https://www.todesk.com/"
 
-  livecheck do
-    url "https://www.todesk.com/download.html"
-    regex(/mac_version\s*[:=]\s*["']?(\d+(?:\.\d+)+)/i)
-  end
+  installer script: {
+    executable: "/bin/bash",
+    args: [
+      "-c",
+      <<~EOS
+        set -e
+        TMP_PKG="/tmp/ToDesk.pkg"
+        /usr/bin/env wget \
+          --progress=bar:force \
+          -U 'Mozilla/5.0' \
+          -O "$TMP_PKG" \
+          https://dl.todesk.com/macos/ToDesk_#{version}.pkg
 
-  pkg "ToDesk_#{version}.pkg"
+        sudo installer -pkg "$TMP_PKG" -target /
+      EOS
+    ]
+  }
 
   uninstall pkgutil: "com.youqu.todesk.mac",
             delete:  "/Applications/ToDesk.app"
